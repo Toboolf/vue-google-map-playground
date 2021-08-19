1bf5fe446538941531e69078883c4a5272af970f
José Cruz <jf.cruz001@ebc.edu.mx>
2021-08-18 19:21:09 -0500

    coding master created google map marker component

---
+++ src/components/GoogleMapMarker.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  props: {
+    google: {
+      type: Object,
+      required: true
+    },
+    map: {
+      type: Object,
+      required: true
+    },
+    marker: {
+      type: Object,
+      required: true
+    },
+    show: {
+      type: Boolean,
+      required: false,
+      default: true
+    }
+  },
+
+  data() { return {
+    self: null
+  }},
+
+  watch: {
+    show() {
+      if (this.show) {
+        this.self.setMap(null)
+      } else {
+        this.self.setMap(this.map)
+      }
+    }
+  },
+
+  mounted() {
+    const { Marker } = this.google.maps;
+
+    this.self = new Marker({
+      position: this.marker.position,
+      marker: this.marker,
+      map: this.map,
+      //icon: POINT_MARKER_ICON_CONFIG
+    });
+  },
+
+  render() { return null }
+};
+</script>
