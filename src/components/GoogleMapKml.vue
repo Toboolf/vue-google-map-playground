184296d401978c9cc7e3a74b3bac69ce5efd1882
Jose Cruz <floobot@hotmail.com>
2021-08-25 02:13:55 -0500

    coding master created kml component

---
+++ src/components/GoogleMapKml.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  props: {
+    google: {
+      type: Object,
+      required: true
+    },
+    map: {
+      type: Object,
+      required: true
+    },
+    src: {
+      type: String,
+      required: true
+    },
+    show: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+  },
+
+  methods: {
+    buildKml() {
+      const KmlLayer = this.google.map.KmlLayer
+      var kmlLayer = new KmlLayer(this.src, {
+        suppressInfoWindows: true,
+        preserveViewport: false,
+        map: this.map
+      })
+      kmlLayer.addListener('click', function(event) {
+        console.log("Cliked my kml layer");
+      });
+    }
+  }
+
+}
+</script>
