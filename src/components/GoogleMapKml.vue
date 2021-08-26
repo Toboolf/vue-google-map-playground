795496c5f5b8726a9d55226211427f8a4856dedc
José Cruz <jf.cruz001@ebc.edu.mx>
2021-08-25 21:07:59 -0500

    coding master adding listener to kml

--- src/components/GoogleMapKml.vue
+++ src/components/GoogleMapKml.vue
@@ -18,21 +18,33 @@ export default {
       required: false,
       default: true
     },
+    container: {
+      type: String,
+      required: true
+    }
   },
 
   methods: {
-    buildKml() {
-      const KmlLayer = this.google.map.KmlLayer
+    buildKml(){
+      const KmlLayer = this.google.maps.KmlLayer
       var kmlLayer = new KmlLayer(this.src, {
         suppressInfoWindows: true,
         preserveViewport: false,
         map: this.map
       })
+      const container = this.container;
       kmlLayer.addListener('click', function(event) {
-        console.log("Cliked my kml layer");
+        var content = event.featureData.infoWindowHtml;
+        var testimonial = document.getElementById(container);
+        testimonial.innerHTML = content;
       });
     }
-  }
+  },
+
+  mounted(){
+    this.buildKml()
+  },
+  render(){ return null }
 
 }
 </script>
